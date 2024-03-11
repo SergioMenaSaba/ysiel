62e5e69ab0f0995cbfae9e56b7e2b6cb5cdcc5b8
SergioMenaSaba <109303985+SergioMenaSaba@users.noreply.github.com>
2024-03-11 23:21:01 +0100

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,10 +1,3 @@
 javascript:(function() {
-    // Crear un array con todos los elementos img en la página
-    var imgElements = document.querySelectorAll('img');
-
-    // Obtener la cantidad de elementos img
-    var imgCount = imgElements.length;
-
-    // Mostrar la cantidad en una alerta
-    alert('Número de elementos img en la página: ' + imgCount);
+  var jsCode = 'alert("Hola Mundo");';
 })();
